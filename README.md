06ba41de47a82f332df79a5a273dd1263f4f7347
Matt Manzi <mmanzi@relaynetwork.com>
2021-07-30 11:49:03 -0400

    Add Transform test and docs

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Stream Responses from Express Server
 
 A proof of concept to continuously stream data from an Express server response to an HTTP client.
+
+## Endpoints
+
+### `/`
+
+Stream data from a [file](./testdata.txt) and pipeline it to the `response` as `text/plain`.
+
+Nothing else fancy happens. However, note that the file being read is only a few lines of text, and `createReadStream` chunks files in [64kiB](https://nodejs.org/docs/latest-v16.x/api/fs.html#fs_fs_createreadstream_path_options) so it's processed as a single chunk.
+
+### `/with-transform`
+
+Same as [`/`](#/) but uses a `Transform` to convert all the text to uppercase. This also tests transmitting the file as multiple chunks by setting the `highWaterMark` to 3 bytes (which ends up being 3 characters at a time).
+
+## Notes
+
+### `BufferEncoding`
+
+When implementing [`transform`](https://nodejs.org/docs/latest-v16.x/api/stream.html#stream_transform_transform_chunk_encoding_callback) in the `Transform` constructor options, there are three parameters that you need to define. The official Node documentation says that the second of these parameters (`encoding`) is a string, representing the encoding of the data chunk _or_ the string `'buffer'` (as a special case). The TypeScript type definitions for this parameter is `BufferEncoding` which covers the normal cases of the string encoding type, but does not cover the possibility that `encoding` may have a value of `'buffer'`. Thus, the type of this parameter should be `BufferEncoding | 'buffer'`.
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createReadStream } from 'fs';
-import { pipeline } from 'stream';
+import { pipeline, Transform } from 'stream';
+import type { TransformCallback } from 'stream';
 
 const PORT = 3000;
 
@@ -10,11 +11,55 @@ const app = express();
 // routes
 app.get('/', (request, response) => {
   console.log('Received request');
-  const file = createReadStream('../testdata.txt');
+  const file = createReadStream('./testdata.txt');
 
   console.log('Writing header');
   response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
   pipeline(file, response, (error) => {
+    if (error) {
+      console.error(error, 'Stream errored');
+    }
+
+    console.log('Ending response');
+    response.end();
+    console.log('Response done');
+  });
+});
+
+app.get('/with-transform', (request, response) => {
+  console.log('Received request');
+  const enc: BufferEncoding = 'utf8';
+  const file = createReadStream('./testdata.txt', {
+    encoding: enc,
+    highWaterMark: 3,
+  });
+
+  const capitalizer = new Transform({
+    transform(
+      chunk: Buffer,
+      encoding: BufferEncoding | 'buffer',
+      callback: TransformCallback,
+    ) {
+      console.log('encoding is', encoding);
+      let chunkStr: string;
+      if (encoding === 'buffer') {
+        chunkStr = chunk.toString(enc);
+      } else {
+        chunkStr = chunk.toString(encoding);
+      }
+
+      console.log('Capitalizing string:', chunkStr);
+      const capped = chunkStr.toUpperCase();
+
+      this.push(capped, enc);
+      callback();
+    },
+  });
+
+  console.log('Writing header');
+  response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+
+  pipeline(file, capitalizer, response, (error) => {
     if (error) {
       console.error(error, 'Stream errored');
     }
