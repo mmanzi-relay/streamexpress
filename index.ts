e4fcb325c4bde3b7aa79651cf9aff19acd09e9b9
Matt Manzi <mmanzi@relaynetwork.com>
2021-07-30 14:18:38 -0400

    Add async pipeline version with transform

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import type { NextFunction, Request, RequestHandler, Response } from 'express';
 import { createReadStream } from 'fs';
 import { pipeline, Transform } from 'stream';
 import type { TransformCallback } from 'stream';
+import { promisify } from 'util';
 
 const PORT = 3000;
 
@@ -9,8 +11,15 @@ const PORT = 3000;
 const app = express();
 
 // routes
+
+//// root
 app.get('/', (request, response) => {
-  console.log('Received request');
+  console.log('Received request', {
+    writable: response.writable,
+    ended: response.writableEnded,
+    finished: response.writableFinished,
+  });
+
   const file = createReadStream('./testdata.txt');
 
   console.log('Writing header');
@@ -26,15 +35,10 @@ app.get('/', (request, response) => {
   });
 });
 
-app.get('/with-transform', (request, response) => {
-  console.log('Received request');
-  const enc: BufferEncoding = 'utf8';
-  const file = createReadStream('./testdata.txt', {
-    encoding: enc,
-    highWaterMark: 3,
-  });
-
-  const capitalizer = new Transform({
+//// root with transform
+const enc: BufferEncoding = 'utf8';
+const getCapitalizer = () =>
+  new Transform({
     transform(
       chunk: Buffer,
       encoding: BufferEncoding | 'buffer',
@@ -56,10 +60,22 @@ app.get('/with-transform', (request, response) => {
     },
   });
 
+app.get('/with-transform', (request, response) => {
+  console.log('Received request', {
+    writable: response.writable,
+    ended: response.writableEnded,
+    finished: response.writableFinished,
+  });
+
+  const file = createReadStream('./testdata.txt', {
+    encoding: enc,
+    highWaterMark: 3,
+  });
+
   console.log('Writing header');
   response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
 
-  pipeline(file, capitalizer, response, (error) => {
+  pipeline(file, getCapitalizer(), response, (error) => {
     if (error) {
       console.error(error, 'Stream errored');
     }
@@ -69,6 +85,47 @@ app.get('/with-transform', (request, response) => {
     console.log('Response done');
   });
 });
+
+//// root with transform as async
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<void>;
+
+const wrap =
+  (handler: AsyncRequestHandler): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) =>
+    handler(req, res, next).catch(next);
+
+const asyncPipeline = promisify(pipeline);
+
+app.get(
+  '/with-transform/async',
+  wrap(async (request, response) => {
+    console.log('A - Received request', {
+      writable: response.writable,
+      ended: response.writableEnded,
+      finished: response.writableFinished,
+    });
+
+    const file = createReadStream('./testdata.txt', {
+      encoding: enc,
+      highWaterMark: 16,
+    });
+
+    console.log('B - Writing header');
+    response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+
+    try {
+      console.log('C - Starting pipeline');
+      await asyncPipeline(file, getCapitalizer(), response);
+      console.log('D - Pipeline done');
+    } catch (error) {
+      console.error(error, 'E - Stream errored');
+    }
+  }),
+);
 
 // start server
 app.listen(PORT, () => {
